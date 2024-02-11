6ca214e7c09334e04250ea45b6ad8fff239f4606
youthOfMe <2040690180@qq.com>
2024-02-11 23:45:36 +0800

    feat: Reflect反射

---
+++ ECMAScript新特性/8.Reflect反射.js
@@ -0,0 +1,13 @@
+/*
+    Reflect反射
+    JavaScript 反射（reflection）是一种能够在运行时检查、修改对象、类和函数等程序结构的能力，
+    通过反射，我们可以读取和修改对象属性、调用对象方法、定义新属性、修改原型等。
+
+    avaScript 通过 Reflect 对象提供了一组操作对象的 API，
+    可以访问、检查和修改对象上的属性和方法。Reflect API 方法与对应对象的同名方法具有相同的功能，
+    例如 Reflect.get() 方法对应对象的 obj.get() 方法，Reflect.set() 方法对应对象 obj.set() 方法等。
+*/
+
+// TODO 反射API示例
+
+// TODO 反射的应用场景
